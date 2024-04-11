f000a8613f4e25cb1f7f3ea97fa87656c2cc98d5
sreelakshmilingamgunta <124713558+sreelakshmilingamgunta@users.noreply.github.com>
2024-04-11 15:42:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
-# variables.tf
-
-variable "resource_group_name" {
-  description = "The name of the resource group."
+variable "vm_name" {
+  description = "The name of the virtual machine."
   type        = string
 }
 
-variable "resource_group_location" {
-  description = "The location of the resource group."
+variable "vm_location" {
+  description = "The location of the virtual machine."
   type        = string
 }
+
+variable "vm_size" {
+  description = "The size of the virtual machine."
+  type        = string
+}
+
+variable "os_disk_size_gb" {
+  description = "The size of the OS disk in gigabytes."
+  type        = number
+}
+
+// Add more variables as needed
