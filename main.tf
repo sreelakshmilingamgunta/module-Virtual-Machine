51c692a22b4d10586ee701bd774a3dec804bfc34
sreelakshmilingamgunta <124713558+sreelakshmilingamgunta@users.noreply.github.com>
2024-04-11 16:12:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,68 @@
-resource "azurerm_resource_group" "example-rg" {
-  name = var.resource_group_name
-  location = "east us"
+resource "azurerm_resource_group" "example" {
+  name     = "sree-resources"
+  location = "West Europe"
 }
-resource "azurerm_virtual_machine" "example" {
-  name                  = var.vm_name
+
+resource "azurerm_virtual_network" "main" {
+  name                = "network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = var.vm_name"
   location              = var.vm_location
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = var.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
-
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
   os_profile_linux_config {
     disable_password_authentication = false
   }
-
-  storage_os_disk {
-    name              = "${var.vm_name}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    disk_size_gb      = var.os_disk_size_gb
-    managed_disk_type = "Premium_LRS"
-  }
-
-  network_interface_ids = [module.network_interface.nic_id]
-
   tags = {
-    environment = "production"
+    environment = "staging"
   }
 }
