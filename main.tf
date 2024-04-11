5ce190dc00099d2dd1abb21c4db74a67f3e85026
sreelakshmilingamgunta <124713558+sreelakshmilingamgunta@users.noreply.github.com>
2024-04-11 16:23:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "sree-resources"
   location = "West Europe"
@@ -30,17 +34,11 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = var.vm_name"
+  name                  = var.vm_name
   location              = var.vm_location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.vm_size
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  vm_size               = var.vm_size # Example VM size
 
   storage_image_reference {
     publisher = "Canonical"
@@ -57,7 +55,7 @@ resource "azurerm_virtual_machine" "main" {
   os_profile {
     computer_name  = "hostname"
     admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_password = "YourStrongPassword123!"  # Replace with a strong password
   }
   os_profile_linux_config {
     disable_password_authentication = false
