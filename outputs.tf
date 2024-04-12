b28c6b535077d4ca13bf834f01af53a5e9ce3208
sreelakshmilingamgunta <124713558+sreelakshmilingamgunta@users.noreply.github.com>
2024-04-12 09:38:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vm_id" {
-  value = azurerm_virtual_machine.example.id
+  value = azurerm_virtual_machine.main.name
 }
 
 output "vm_ip_address" {
-  value = azurerm_virtual_machine.example.private_ip_address
+  value = azurerm_virtual_machine.main.private_ip_address
 }
 
 // Add more outputs as needed
